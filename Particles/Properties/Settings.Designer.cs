5329fbad855bf6da7488f83013c2ac8586c79c23
gro-ser <seregaaa33@ya.ru>
2020-09-06 15:52:51 +0300

    Update .gitignore

--- Particles/Properties/Settings.Designer.cs
+++ Particles/Properties/Settings.Designer.cs
@@ -8,29 +8,36 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Particles.Properties {
-    
-    
+namespace Particles.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
             }
         }
-        
+
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool DebuggerLabels {
-            get {
+        public bool DebuggerLabels
+        {
+            get
+            {
                 return ((bool)(this["DebuggerLabels"]));
             }
-            set {
+            set
+            {
                 this["DebuggerLabels"] = value;
             }
         }
